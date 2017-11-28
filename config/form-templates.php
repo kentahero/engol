e53f29840dd2a4efaae9007786965b084fb8a6e3
matsu <matsu@DESKTOP-U6BQQMM>
2017-11-28 16:26:29 +0900

    ラジオボタンの機種依存問題を対応

--- config/form-templates.php
+++ config/form-templates.php
@@ -2,7 +2,7 @@
 return [
 	'radio' => '<input class="radio" type="radio" name="{{name}}" value="{{value}}"{{attrs}}>',
 	'error' => '<span class="error-msg">{{content}}</span>',
-	'nestingLabel' => '<label class="radio bvc" {{attrs}}>{{hidden}}{{input}}{{text}}</label>',
+	'nestingLabel' => '<label class="radio bvc" {{attrs}} onclick="">{{hidden}}{{input}}{{text}}</label>',
 	//'select' => '<span class="select"><select name="{{name}}" class="select" {{attrs}} >{{content}}</select></span>',
 	//'radioWrapper' => '<div class="wrapper-radio">{{label}}</div>',
 ];
